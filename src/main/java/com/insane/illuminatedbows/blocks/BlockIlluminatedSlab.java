f274972820b17f7e4a7852a91260c5ab030df987
Michael Lydeamore <michael.lydeamore@adelaide.edu.au>
2014-11-07 16:54:03 +1030

    Slabs are no longer a normal cube

--- src/main/java/com/insane/illuminatedbows/blocks/BlockIlluminatedSlab.java
+++ src/main/java/com/insane/illuminatedbows/blocks/BlockIlluminatedSlab.java
@@ -49,6 +49,12 @@ public class BlockIlluminatedSlab extends Block {
     public IIcon getIcon(int par1, int par2) {
         return this.blockIcon;
     }
+    
+    @Override
+    public boolean isNormalCube()
+    {
+    	return false;
+    }
 
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
